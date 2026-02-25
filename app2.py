4549b6156ab422a81d6aca99300d857e002aff0e
karleaabha-byte <karle.aabha@gmail.com>
2026-02-25 12:31:33 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix
